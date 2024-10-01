12c4e7897b5526da0569b40b493f554030e28ce0
unknown <jonocribb@gmail.com>
2024-10-02 01:09:54 +1000

    KemoNine remix starting point

--- users/ardux/layout/remixes/remix_layers.h
+++ users/ardux/layout/remixes/remix_layers.h
@@ -1,84 +1,65 @@
-// Copyright (c) 2022 purple-rw
+// Copyright (c) 2021 Mike "KemoNine" Crosson
 // SPDX-License-Identifier: Apache-2.0
+#pragma once
 
-#define ARTSEY_STD_LAYER_PAR_LEFT \
+//////////
+// std ardux custom layer
+#define ARDUX_STD_LAYER_CUS_LEFT \
 STD_LEADING_NONES \
-KC_LCBR,          KC_LPRN,          KC_RPRN,          KC_TRNS,          \
+KC_PSCR,            KC_VOLU,          KC_WWW_HOME,        KC_WWW_BACK, \
 STD_BETWEEN_ROW_ONE_TWO_NONES \
-KC_RCBR,          KC_LBRC,          KC_RBRC,          KC_NO             \
+KC_TRNS,          KC_VOLD,          KC_WWW_SEARCH,      KC_MUTE      \
 STD_TRAILING_NONES
 
-#define ARTSEY_STD_LAYER_SYM_LEFT \
-STD_LEADING_NONES \
-KC_DLR,           KC_ASTR,          KC_UNDS,          KC_EXLM,          \
-STD_BETWEEN_ROW_ONE_TWO_NONES \
-KC_HASH,          KC_EQL,           KC_MINS,          _______           \
-STD_TRAILING_NONES
-
-#define ARTSEY_STD_LAYER_CUS_LEFT \
-STD_LEADING_NONES \
-_______,          KC_VOLD,          KC_MUTE,          KC_VOLU,          \
-STD_BETWEEN_ROW_ONE_TWO_NONES \
-XXXXXXX,          XXXXXXX,          XXXXXXX,          KC_PSCR           \
-STD_TRAILING_NONES
-
-#define ARTSEY_STD_LAYER_FUN_LEFT \
-STD_LEADING_NONES \
-KC_F1,            KC_F2,            KC_F3,            KC_F4,            \
-STD_BETWEEN_ROW_ONE_TWO_NONES \
-KC_F5,            KC_F6,            KC_F7,            KC_F8             \
-STD_TRAILING_NONES
-
-#define ARTSEY_STD_LAYER_RGB_LEFT \
-STD_LEADING_NONES \
-KRGB_T4,          KRGB_T3,          KRGB_T2,          KRGB_T1,          \
-STD_BETWEEN_ROW_ONE_TWO_NONES \
-KRGB_B4,          KRGB_B3,          KRGB_B2,          KRGB_B1           \
-STD_TRAILING_NONES
-
-#define ARTSEY_STD_LAYER_PAR_RIGHT \
-STD_LEADING_NONES \
-_______,          KC_LPRN,          KC_RPRN,          KC_LCBR,          \
-STD_BETWEEN_ROW_ONE_TWO_NONES \
-XXXXXXX,          KC_LBRC,          KC_RBRC,          KC_RCBR           \
-STD_TRAILING_NONES
-
-#define ARTSEY_STD_LAYER_SYM_RIGHT \
-STD_LEADING_NONES \
-KC_EXLM,          KC_UNDS,          KC_ASTR,          KC_DLR,           \
-STD_BETWEEN_ROW_ONE_TWO_NONES \
-_______,          KC_MINS,          KC_EQL,           KC_HASH           \
-STD_TRAILING_NONES
-
-#define ARTSEY_STD_LAYER_CUS_RIGHT \
-STD_LEADING_NONES \
-KC_VOLD,          KC_MUTE,          KC_VOLU,          _______,          \
-STD_BETWEEN_ROW_ONE_TWO_NONES \
-KC_PSCR,          XXXXXXX,          XXXXXXX,          XXXXXXX           \
-STD_TRAILING_NONES
-
-#define ARTSEY_STD_LAYER_FUN_RIGHT \
-STD_LEADING_NONES \
-KC_F1,            KC_F2,            KC_F3,            KC_F4,            \
-STD_BETWEEN_ROW_ONE_TWO_NONES \
-KC_F5,            KC_F6,            KC_F7,            KC_F8             \
-STD_TRAILING_NONES
-
-#define ARTSEY_STD_LAYER_RGB_RIGHT \
-STD_LEADING_NONES \
-KRGB_T1,          KRGB_T2,          KRGB_T3,          KRGB_T4,          \
-STD_BETWEEN_ROW_ONE_TWO_NONES \
-KRGB_B1,          KRGB_B2,          KRGB_B3,          KRGB_B4           \
-STD_TRAILING_NONES
-
-#ifdef ARTSEY_SIZE_STD
-// Allow proper auto selection of left / right via keymaps
-#ifdef ARTSEY_HAND_LEFT
-#define ARTSEY_STD_LAYER_FUN ARTSEY_STD_LAYER_FUN_LEFT
-#define ARTSEY_STD_LAYER_RGB ARTSEY_STD_LAYER_RGB_LEFT
+//////////
+// big ardux custom layer
+#ifndef ARDUX_BIG_LAYER_CUS_LEFT
+#ifdef ARDUX_TWO_THUMB
+#define ARDUX_BIG_LAYER_CUS_LEFT_THUMBS KC_NO, KC_NO
 #endif
-#ifdef ARTSEY_HAND_RIGHT
-#define ARTSEY_STD_LAYER_FUN ARTSEY_STD_LAYER_FUN_RIGHT
-#define ARTSEY_STD_LAYER_RGB ARTSEY_STD_LAYER_RGB_RIGHT
+#ifndef ARDUX_TWO_THUMB
+#define ARDUX_BIG_LAYER_CUS_LEFT_THUMBS KC_NO, KC_NO, KC_NO
 #endif
-#endif // ARTSEY_SIZE_STD
+#endif
+
+#define ARDUX_BIG_LAYER_CUS_LEFT \
+BIG_LEADING_NONES \
+KC_PSCR,            KC_VOLU,          KC_WWW_HOME,           KC_WWW_BACK,          KC_NO,            \
+BIG_BETWEEN_ROW_ONE_TWO_NONES \
+KC_TRNS,          KC_VOLD,          KC_WWW_SEARCH,          KC_MUTE,          KC_NO,            \
+BIG_BETWEEN_ROW_TWO_THREE_NONES \
+KC_NO,            KC_NO,            KC_NO,            KC_NO,            KC_NO,            \
+BIG_BETWEEN_ROW_THREE_THUMBS_NONES \
+ARDUX_BIG_LAYER_CUS_LEFT_THUMBS \
+BIG_TRAILING_NONES
+
+//////////
+// big ardux number layer
+#ifndef ARDUX_BIG_LAYER_NUM_LEFT
+#ifdef ARDUX_TWO_THUMB
+#define ARDUX_BIG_LAYER_NUM_LEFT_THUMBS KC_UNDS, KC_SPACE
+#endif
+#ifndef ARDUX_TWO_THUMB
+#define ARDUX_BIG_LAYER_NUM_LEFT_THUMBS KC_TAB, KC_UNDS, KC_SPACE
+#endif
+#endif
+
+#define ARDUX_BIG_LAYER_NUM_LEFT \
+BIG_LEADING_NONES \
+KNUM_T4,          KNUM_T3,          KNUM_T2,          KNUM_T1,          KC_MINUS,            \
+BIG_BETWEEN_ROW_ONE_TWO_NONES \
+KNUM_B4,          KNUM_B3,          KNUM_B2,          KNUM_B1,          KC_SLASH,            \
+BIG_BETWEEN_ROW_TWO_THREE_NONES \
+KC_NO,            KC_NO,            KC_NO,            KC_TAB,            KC_COLON,            \
+BIG_BETWEEN_ROW_THREE_THUMBS_NONES \
+ARDUX_BIG_LAYER_NUM_LEFT_THUMBS \
+BIG_TRAILING_NONES
+
+//////////
+// big ardux mouse layer
+#define ARDUX_BIG_LAYER_MSE_LEFT_THUMBS
+#define ARDUX_BIG_LAYER_MSE_LEFT \
+KC_NO, KMSE_T4, KMSE_T3, KMSE_T2, KMSE_T1, KC_NO,        KC_NO, KMSE_T1, KMSE_T2,  KMSE_T3, KMSE_T4, KC_NO, \
+KC_NO, KMSE_B4, KMSE_B3, KMSE_B2, KMSE_B1, KC_NO,        KC_NO, KMSE_B3, KMSE_B2,  KMSE_B1, KMSE_B4, KC_NO, \
+KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,        KC_NO, KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO, \
+                         KC_NO,   KC_NO,   KC_NO,        KC_NO, KC_NO, KC_NO
