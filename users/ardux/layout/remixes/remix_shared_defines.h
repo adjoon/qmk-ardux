0adf3a860672281a08f601c6d151c63ead6eaf7c
unknown <jonocribb@gmail.com>
2024-10-03 01:09:23 +1000

    control and mod key changes + swap to ONISARET base

--- users/ardux/layout/remixes/remix_shared_defines.h
+++ users/ardux/layout/remixes/remix_shared_defines.h
@@ -4,6 +4,7 @@
 #define DISABLE_DEFAULT_NUMBER_COMBOS
 #define DISABLE_DEFAULT_ALPHA_COMBOS
 #define DISABLE_DEFAULT_CONTROL_COMBOS
+#define DISABLE_DEFAULT_MOD_COMBOS
 
 // Colemak base keys
 #undef KEY_T1
@@ -16,11 +17,11 @@
 #undef KEY_B4
 #define KEY_B4 LT(LAYER_ID_NUMBERS, KC_A)
 #define KEY_B3 KC_R
-#define KEY_B2 KC_S
+#define KEY_B2 KC_E
 #define KEY_B1 LT(LAYER_ID_SYMBOLS, KC_T)
-#define KEY_T1 LT(LAYER_ID_PARENTHETICALS, KC_N)
-#define KEY_T2 KC_E
-#define KEY_T3 KC_I
+#define KEY_T1 LT(LAYER_ID_PARENTHETICALS, KC_S)
+#define KEY_T2 KC_I
+#define KEY_T3 KC_N
 #define KEY_T4 LT(LAYER_ID_CUSTOM, KC_O)
 
 // Ten-key number keys (Left Hand Based)
